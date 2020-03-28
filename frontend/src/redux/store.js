573e72c9adf02f2db96977d478d81a135d52a07e
Napat07 <naphat1811@gmail.com>
2020-03-28 23:18:36 +0700

    updated

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -7,41 +7,14 @@ const initial = {
     img: ''
 }
 
-const bearReducer = (bears = [], action) => {
 
-    switch (action.type) {
-        case 'GET_BEARS':
-            return action.bears
-        case 'ADD_BEARS':
-            return [...bears, { id: bears.length, ...action.bear }]
-        case 'DELETE_BEARS':
-            return bears.filter(bear => +bear.id !== +action.id)
-        case 'UPDATE_BEARS':
-            return bears.map(bear => {
-                if (+bear.id === +action.id)
-                    return action.bear
-                else
-                    return bear
-            })
-    }
-
-    return bears
-}
 
 const formReducer = (data = initial, action) => {
     switch (action.type) {
-        case 'CHANGE_NAME':
-            return { ...data, name: action.name }
-        case 'CHANGE_WEIGHT':
-            return { ...data, weight: action.weight }
-        case 'CHANGE_IMG':
-            return { ...data, img: action.img }
-        case 'INITIAL':
-            return {
-                name: '',
-                weight: '',
-                img: ''
-            }
+        case 'CHANGE_NAME':return { ...data, name: action.name }
+        case 'CHANGE_WEIGHT':return { ...data, weight: action.weight }
+        case 'CHANGE_IMG':return { ...data, img: action.img }
+        case 'INITIAL': return { name: '', weight: '', img: ''}
         case 'CHANGE':
             console.log(action)
             return {
@@ -54,7 +27,22 @@ const formReducer = (data = initial, action) => {
     return data
 }
 
+const bearReducer = (bears = [], action) => {
 
+    switch (action.type) {
+        case 'GET_BEARS': return action.bears
+        case 'ADD_BEARS': return [...bears, { id: bears.length, ...action.bear }]
+        case 'DELETE_BEARS': return bears.filter(bear => +bear.id !== +action.id)
+        case 'UPDATE_BEARS': return bears.map(bear => {
+                if (+bear.id === +action.id)
+                    return action.bear
+                else
+                    return bear
+            })
+    }
+
+    return bears
+}
 
 const rootReducer = combineReducers({
     bears: bearReducer,
